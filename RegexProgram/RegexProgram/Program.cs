c222705604892379eb25a19c522976aa15dd5ef9
Princekotadiya007 <princuuu77@gmail.com>
2023-02-28 08:41:04 +0530

    [prince]:add Enter a valid first name

---
+++ RegexProgram/RegexProgram/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegexProgram
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            RegexPrograms regex = new RegexPrograms();
+            regex.ValidateFirstName("Prince");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RegexProgram/RegexProgram/RegexPrograms.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexProgram
+{
+    public class RegexPrograms
+    {
+        public void ValidateFirstName(string name)
+        {
+            string firstName = "^[A-Z][a-z]{3,}?";
+            if (Regex.IsMatch(name, firstName))
+            {
+                Console.WriteLine("First name is matching with Regex");
+            }
+            else
+            {
+                Console.WriteLine("First name is not matcging with Regex");
+            }
+        }
+    }
+}
